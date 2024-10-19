ef4c34a663452760d182fbca698b2251f12418ca
test-ilya <asdnone@yandex.ru>
2024-10-19 16:19:16 +0300

    modified:   playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,15 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'],
+        viewport: {
+          "width": 1920,
+          "height": 1080
+        },
+        locale: 'ru-RU'
+      },
+      
     },
 
     // {
