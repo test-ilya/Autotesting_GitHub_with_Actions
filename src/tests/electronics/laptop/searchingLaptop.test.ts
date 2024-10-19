b6b24c746be8a170c89bf033a9ee4d071c731ff3
test-ilya <asdnone@yandex.ru>
2024-10-19 16:27:10 +0300

    modified:   src/tests/electronics/laptop/searchingLaptop.test.ts

--- src/tests/electronics/laptop/searchingLaptop.test.ts
+++ src/tests/electronics/laptop/searchingLaptop.test.ts
@@ -8,9 +8,12 @@ test('Searching laptop - Apple MacBook Air 13', async ({ page }) => {
 
     await page.goto('/');
     console.log('Open main page');
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
     await laptopPage.itemSearch(laptopName);
     console.log('Do search');
+    await page.screenshot({ path: 'screenshot2.png', fullPage: true });
+
 
     await expect(
         page.locator(laptopPage.searchResultSelector)
