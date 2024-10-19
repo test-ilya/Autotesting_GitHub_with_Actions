340331e8a3b6bafb34271a25fb24323a5335b3ab
test-ilya <asdnone@yandex.ru>
2024-10-19 16:38:03 +0300

    deleted:    screenshot.png 	deleted:    screenshot2.png 	modified:   src/tests/electronics/laptop/searchingLaptop.test.ts

--- src/tests/electronics/laptop/searchingLaptop.test.ts
+++ src/tests/electronics/laptop/searchingLaptop.test.ts
@@ -6,14 +6,11 @@ const laptopName = 'Apple MacBook Air 13';
 test('Searching laptop - Apple MacBook Air 13', async ({ page }) => {
     const laptopPage = new LaptopPage(page);
 
-    await page.goto('/');
+    await page.goto('https://market.yandex.ru');
     console.log('Open main page');
-    await page.screenshot({ path: 'screenshot.png', fullPage: true });
-
+    
     await laptopPage.itemSearch(laptopName);
     console.log('Do search');
-    await page.screenshot({ path: 'screenshot2.png', fullPage: true });
-
 
     await expect(
         page.locator(laptopPage.searchResultSelector)
